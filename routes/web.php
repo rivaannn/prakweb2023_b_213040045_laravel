4d0875e641a0ea9aed24df82a42a8aa93c26292b
rivaannn <mrivans2002@gmail.com>
2023-10-27 12:53:16 +0700

    Baru Mengerjakan Video Laravel - 4

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,66 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Muhamad Rivan Sahronie",
         "email" => "rivan.213040045@mail.unpas.ac.id",
         "image" => "fotovan.jpg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view('pots');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rivan Sahronie",
+            "body" => "Lorem ipsum dolor sit amet consectetur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Rivan Sahronie",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman Single Post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rivan Sahronie",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium et officia voluptatum ducimus dignissimos libero tempore at dolorem aspernatur! Ad eligendi doloremque tempore assumenda necessitatibus, recusandae eum perferendis accusamus fuga porro repellat perspiciatis dolorem beatae dolor modi minima nostrum ut voluptates, nemo a? Quaerat ex obcaecati amet ratione repudiandae laudantium."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Rivan Sahronie",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos cupiditate saepe vitae sit, ipsam hic nobis iusto perferendis, molestiae placeat iste necessitatibus, doloremque cumque culpa minus facilis! Officia ea ratione esse. Quasi, nisi hic dicta deserunt eaque maxime autem reprehenderit!"
+        ],
+    ];
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
