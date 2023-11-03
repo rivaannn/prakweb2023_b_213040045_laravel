830e1835389cdd18a4b6153b5b35cac097f82a6a
rivaannn <mrivans2002@gmail.com>
2023-11-04 00:30:38 +0700

    Baru Mengerjakan Video Laravel - 12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Web Programming',
             'slug' => 'web-programming'
         ]);
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
 
         Category::create([
             'name' => 'Personal',
