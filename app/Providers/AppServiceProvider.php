69e7db6d2d92319b6e28eccafeb00f8afe35d97d
rivaannn <mrivans2002@gmail.com>
2023-11-24 00:57:48 +0700

    Baru Mengerjakan Video Laravel - 23

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 // use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin;
+        });
     }
 }
--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -16,5 +16,20 @@
                 </a>
             </li>
         </ul>
+
+        @can('admin')
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                <span>Administrator</span>
+            </h6>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }} text-decoration-none text-dark"
+                        href="/dashboard/categories">
+                        <i data-feather="grid"></i>
+                        Post Categories
+                    </a>
+                </li>
+            </ul>
+        @endcan
     </div>
 </nav>
